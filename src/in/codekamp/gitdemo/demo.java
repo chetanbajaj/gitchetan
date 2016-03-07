192bd5e0b73120970f0dd674168315954ac65e36
chetan jain <chetan27june@gmail.com>
2016-03-07 17:58:41 +0530

    initial project setup

---
+++ src/in/codekamp/gitdemo/demo.java
@@ -0,0 +1,16 @@
+package in.codekamp.gitdemo;
+
+/**
+ * Created by CHETAN JAIN on 07-03-2016.
+ */
+public class demo {
+//git init
+    //git status
+   // git config --global user.name"name here "
+    //creat a.gitignorefile in the root directory
+
+
+    /// git add <file path >
+    //
+
+}
